6dfc443a3449e49633bd9ea9e7efa20fe283d064
Jan K <jan.krusnik@tum.de>
2021-05-27 20:23:26 +0200

    ResourcesDir test

--- Sources/ApodiniLeaf/LeafView.swift
+++ Sources/ApodiniLeaf/LeafView.swift
@@ -13,18 +13,20 @@ public struct LeafView: ResponseTransformable {
     private let path: String
     private let information: [InformationKey: String]
     private let fileio: NonBlockingFileIO
+    private let resourcesDir: String
     
-    public init(_ path: String, _ context: [String: LeafData]?, information: [InformationKey: String]?, fileio: NonBlockingFileIO) {
+    public init(_ path: String, _ context: [String: LeafData]?, information: [InformationKey: String]?, fileio: NonBlockingFileIO, resourcesDir: String) {
         self.path = path
         self.context = context ?? [:]
         self.information = information ?? [:]
         self.fileio = fileio
+        self.resourcesDir = resourcesDir
     }
     
     public func transformToResponse(on eventLoop: EventLoop) -> EventLoopFuture<Response<Raw>> {
-        let sources = LeafSources.singleSource(NIOLeafFiles(fileio: fileio))
+        let sources = LeafSources.singleSource(NIOLeafFiles(fileio: fileio, limits: .onlyLeafExtensions, sandboxDirectory: resourcesDir, viewDirectory: resourcesDir, defaultExtension: "leaf"))
         
-        let renderer = LeafRenderer(configuration: LeafConfiguration(rootDirectory: "/Resources/Views/"), sources: sources, eventLoop: eventLoop)
+        let renderer = LeafRenderer(configuration: LeafConfiguration(rootDirectory: resourcesDir), sources: sources, eventLoop: eventLoop)
         
         return renderer.render(path: path, context: context)
             .map { renderedHTML in
