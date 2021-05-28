19ef4f2799b8251c5b630406e19dece0eb4b6eb8
Jan K <jan.krusnik@tum.de>
2021-05-28 10:52:55 +0200

    Add LeafEncoder from Leaf

--- Sources/ApodiniLeaf/LeafView.swift
+++ Sources/ApodiniLeaf/LeafView.swift
@@ -8,13 +8,13 @@
 import LeafKit
 import Apodini
 
-public struct LeafView: ResponseTransformable {
-    private let context: [String: LeafData]
+public struct LeafView<Context>: ResponseTransformable where Context: Encodable {
     private let path: String
     private let renderer: LeafRenderer
+    private let context: Context?
     private let information: [InformationKey: String]
     
-    public init(_ path: String, with renderer: LeafRenderer, context: [String: LeafData] = [:], information: [InformationKey: String] = [:]) {
+    public init(_ path: String, with renderer: LeafRenderer, context: Context? = nil, information: [InformationKey: String] = [:]) {
         self.path = path
         self.context = context
         self.renderer = renderer
@@ -22,7 +22,15 @@ public struct LeafView: ResponseTransformable {
     }
     
     public func transformToResponse(on eventLoop: EventLoop) -> EventLoopFuture<Response<Raw>> {
-        renderer.render(path: path, context: context)
+        let data: [String: LeafData]
+        
+        do {
+            data = try LeafEncoder().encode(context)
+        } catch {
+            return renderer.eventLoop.makeFailedFuture(error)
+        }
+        
+        return renderer.render(path: path, context: data)
             .map { renderedHTML in
                 let raw = Raw(renderedHTML, type: MimeType.text(.html, parameters: [:]))
                 return Response.send(raw, information: information)
